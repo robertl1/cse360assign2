9b2e176f4d05124aa811c0c3871031cea11d7ee9
Robert Le <robertl1@asu.edu>
2019-10-09 20:12:42 -0700

    Initial check in for Adding Machine assignment 2.

---
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	
+	/**
+	 * This is a constructor that initializes the total variable to 0.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * Returns the total value of total.
+	 * But for now it is returning 0.
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The method adds the parameter value to the total.
+	 * @param value the value to be added to total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * The method subtracts the value given from the total variable.
+	 * @param value to be subtracted from total.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * This method will return a history of method commands to the user.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clearing the AddingMachine variables.
+	 */
+	public void clear() {
+	
+	}
+}
